87e981b061d824290ce7848f58b47d6a418816d4
stu-keil <stephanekeil@gmail.com>
2016-04-04 18:29:51 -0500

    2.c y 3.c concepto de promotion

---
+++ 2.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main(void){
+char caracter;
+int num;
+caracter = '\0';
+//comentario
+num = 0;
+printf("caracter nulo: %c\n",caracter);
+printf("numero: %d\n",num);
+printf("COmbina: %d\n",num+caracter);
+return 23;
+}
---
+++ 3.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(void){
+char caracter;
+int num;
+caracter = 'a';
+//comentario
+num = 10;
+printf("caracter: %c\n",caracter);
+printf("numero: %d\n",num);
+printf("COmbina: %d\n",num+caracter);
+printf("Valor de caracter %d\n",caracter);
+return 23;
+}
